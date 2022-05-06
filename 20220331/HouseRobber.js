4a52b56a9f78d7ee9a9672357542888cfbf5d647
Carbon Kuo <carbonkuo@gmail.com>
2022-05-06 22:02:17 +0800

    Edit the question-solving direction

--- 20220331/HouseRobber.js
+++ 20220331/HouseRobber.js
@@ -1,11 +1,11 @@
 /** 
  * LeetCode 198. House Robber
  *
- * maxMoney用來記錄偷到每一家所偷到最高現金，
- * maxMoney[i]則是偷到第i家時所偷得最高現金。
- * 這一家偷不偷由nums[i]、maxMoney[i - 1]、maxMoney[i - 2]來決定。
- * 如果nums[i] + maxMoney[i - 2] > maxMoney[i - 1] 則偷，即maxMoney[i] = nums[i] + maxMoney[i - 2]；
- * 反之不偷，即maxMoney[i] = maxMoney[i - 1]。
+ * maxMoney is used to record the highest cash stolen from each house,
+ * maxMoney[i] is the highest amount of cash stolen from the i-th house.
+ * Whether this house steals is determined by nums[i], maxMoney[i - 1], and maxMoney[i - 2].
+ * Steal if nums[i] + maxMoney[i - 2] > maxMoney[i - 1], i.e. maxMoney[i] = nums[i] + maxMoney[i - 2];
+ * Otherwise do not steal, i.e. maxMoney[i] = maxMoney[i - 1].
  * maxMoney[i] = Math.max(maxMoney[i - 1], nums[i] + maxMoney[i - 2])
  * maxMoney[ i + 1 ] = Math.max(maxMoney[ i ], nums[ i ] + (maxMoney[ i - 1 ] ?? 0) )
  * 
