0b314b8f04767e1786ca6a0d0e1f7aebde136dff
Carbon Kuo <carbonkuo@gmail.com>
2022-05-08 00:00:08 +0800

    add 0506 code

--- 20220503/twoSum.js
+++ 20220503/twoSum.js
@@ -21,6 +21,18 @@ var twoSum = function(nums, target) {
 };
 
 // var twoSum = function(nums, target) {
+//     let map = new Map();
+//     for(let i = 0; i < nums.length; i++){
+//         if(map.has(target - nums[i])){
+//             return [map.get(target - nums[i]), i]
+//         }else{
+//             map.set(nums[i], i);
+//         }
+//     }
+//     return [];
+// };
+
+// var twoSum = function(nums, target) {
 //   let res = [];
 //   for(let i = 0; i < nums.length; i++){
 //     for(let j = i + 1; j < nums.length; j++){
---
+++ 20220506/find132pattern.js
@@ -0,0 +1,40 @@
+/**  LeetCode 456. 132 Pattern
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function(nums) {
+  const elements = nums.length;
+  if(elements < 3) return false;
+  let stack = [];
+  let med = -Infinity;
+  
+  for( let i = elements - 1; i >= 0; --i){
+    if(nums[i] < med) return true;
+    while(stack.length > 0 && nums[i] > stack[stack.length - 1]){
+      med = stack.pop();
+    }
+    stack.push(nums[i]);
+  }
+  return false;
+}
+
+
+
+
+// Runtime Error
+/**
+  var find132pattern = function(nums) {
+    let hasPattern = [];
+      for(let i = 0; i < nums.length; i++){
+        let min = nums[i];
+        for(let j = i + 1; j < nums.length; j++){
+          for( let k = j + 1; k < nums.length; k++){
+            if(nums[j] > nums[k] && nums[k] > min){
+              hasPattern.push([min, nums[j], nums[k]]);
+            }
+          }
+        }
+      }
+    return hasPattern.length === 0 ? false : true;
+  };
+ */
