e75ef241176aaef25c6d45a58b7cbf09dfb0b4c7
Carbon Kuo <carbonkuo@gmail.com>
2022-05-09 01:27:35 +0800

    add coding day 20220508

---
+++ 20220508/findDuplicates.js
@@ -0,0 +1,34 @@
+/** LeetCode 442. Find All Duplicates in an Array
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var findDuplicates = function(nums) {
+  let res = [];
+  let counts = [];
+  for(let num of nums){
+    if(counts[num]){
+      counts[num]++;
+    }else{
+      counts[num] = 1;
+    }
+  }
+  for(let i = 0; i <= counts.length; i++){
+    if(counts[i] === 2){
+      res.push(i);
+    }
+  }
+  return res;
+};
+
+// var findDuplicates = function(nums) {
+//   let res = [];
+//   for (let i = 0; i < nums.length; i++){
+//     let target = Math.abs(nums[i]);
+//     if(nums[target - 1] > 0){
+//       nums[target - 1] = -nums[target - 1]; 
+//     }else{
+//       res.push(target);
+//     }
+//   }
+//   return res;
+// };
---
+++ 20220508/findReplaceString.js
@@ -0,0 +1,39 @@
+/** LeetCode 833. Find And Replace in String
+ * @param {string} s
+ * @param {number[]} indices
+ * @param {string[]} sources
+ * @param {string[]} targets
+ * @return {string}
+ */
+ var findReplaceString = function(s, indices, sources, targets) {
+  let resArray = s.split('');
+  for (let i = 0; i < indices.length; i++){
+    let replaceString = sources[i];
+    let checkString = s.slice(indices[i], indices[i] + replaceString.length);
+    if (checkString === replaceString){
+      let temp = new Array(replaceString.length).fill('');
+      temp[0] = targets[i];
+      resArray.splice(indices[i], replaceString.length, ...temp);
+    }
+  }
+  return resArray.join('');
+};
+
+
+//  var findReplaceString = function(s, indices, sources, targets) {
+//   let resArray = s.split('');
+//   for (let i = 0; i < indices.length; i++){
+//     let replaceString = sources[i];
+//     let replaceIndex = s.indexOf(replaceString, 0);
+//     while (replaceIndex > -1) {
+//       if (replaceIndex === indices[i]) {
+//         let temp = new Array(replaceString.length).fill(' ');
+//         temp[0] = targets[i];
+//         resArray.splice(replaceIndex, replaceString.length, ...temp);
+//         break;
+//       }
+//       replaceIndex = s.indexOf(replaceString, replaceIndex + 1);
+//     }
+//   }
+//   return resArray.join('').split(' ').join('');
+// };
