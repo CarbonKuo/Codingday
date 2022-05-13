c9c70f8cefd72a6d1ab137c0142555dd42589079
Carbon Kuo <carbonkuo@gmail.com>
2022-05-13 23:33:54 +0800

    edit coding day 20220513

--- 20220512/minAddToMakeValid.js
+++ 20220512/minAddToMakeValid.js
@@ -20,3 +20,23 @@
   }
   return needOpeningParenthesis + needClosingParenthesis;
 };
+
+/** LeetCode 921. Minimum Add to Make Parentheses Valid
+ * @param {string} Parentheses
+ * @return {number} needOpeningParenthesis + needClosingParenthesis
+ */
+var minAddToMakeValid = function(Parentheses) {
+  let res = Parentheses.length;
+  let needClosingParenthesis = 0;
+
+  for (let Parenthesis of Parentheses) {
+    if (Parenthesis === '(') {
+      needClosingParenthesis++;
+    }
+    else if(needClosingParenthesis > 0) {
+      needClosingParenthesis--;
+      res -= 2;
+    }
+  }
+  return res;
+};
