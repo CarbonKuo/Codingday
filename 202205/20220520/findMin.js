099fcc97793d217f2df18f62cf968a3552a010dc
Carbon Kuo <carbonkuo@gmail.com>
2022-05-22 02:49:32 +0800

    push coding day 20220520

--- 202205/20220520/findMin.js
+++ 202205/20220520/findMin.js
@@ -6,7 +6,7 @@ var findMin = function(nums) {
   let left = 0;
   let right = nums.length - 1;
   while (left < right) {
-    let mid = Math.floor((left + right)/2)
+    let mid = Math.floor((left + right)/2);
     if (nums[mid] > nums[right]){
       left = mid + 1; 
     }
