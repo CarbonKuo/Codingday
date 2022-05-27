1550d023c685ce277105fd7a07f2abd136f96f1b
Carbon Kuo <carbonkuo@gmail.com>
2022-05-27 11:49:32 +0800

    add new solution in coding day 20220525

--- 202205/20220525/hammingWeight.js
+++ 202205/20220525/hammingWeight.js
@@ -3,10 +3,25 @@
  * @return {number} Weight
  */
  var hammingWeight = function(n) {
-  let bit = n.toString(2);
   let Weight = 0;
-  for (let i = 0; i < bit.length; i++){
-    if(bit[i] === '1') Weight += 1;
+  while(n > 0){
+    if(n % 2 === 1){
+      Weight++;
+    }
+    n = Math.floor(n / 2);
   }
   return Weight;
 };
+
+/** LeetCode 191. Number of 1 Bits
+ * @param {number} n - a positive integer
+ * @return {number} Weight
+ */
+//  var hammingWeight = function(n) {
+//   let bit = n.toString(2);
+//   let Weight = 0;
+//   for (let i = 0; i < bit.length; i++){
+//     if(bit[i] === '1') Weight += 1;
+//   }
+//   return Weight;
+// };
