dceec815b654f9a4e6bbfd52bfc7211002410a21
Carbon Kuo <carbonkuo@gmail.com>
2022-06-08 13:16:37 +0800

    add 0035.Search Insert Position

---
+++ LeetCode/0035.Search Insert Position/test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('assert');
+
+const { searchInsert } = require('./');
+
+describe('#searchInsert', () => {
+  const tests = [
+    {
+      nums: [1, 3, 5, 6],
+      target: 5,
+      result: 2,
+    },
+    {
+      nums: [1, 3, 5, 6],
+      target: 2,
+      result: 1,
+    },
+    {
+      nums: [1, 3, 5, 6],
+      target: 7,
+      result: 4,
+    },
+  ];
+
+  for (const { nums, target, result } of tests) {
+    it(`${nums}, ${target} -> ${result}`, () => {
+      assert.deepStrictEqual(searchInsert(nums, target), result);
+    });
+  }
+});
