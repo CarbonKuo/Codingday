903f49dd8d87971881fa3b1aca27e235e5c5362d
Carbon Kuo <carbonkuo@gmail.com>
2022-06-15 11:22:52 +0800

    fix lint

--- CodingDay2022_May/18th/criticalConnections.js
+++ CodingDay2022_May/18th/criticalConnections.js
@@ -3,27 +3,28 @@
  * @param {number[][]} connections
  * @return {number[][]} criticalConnection
  */
- var criticalConnections = function(n, connections) {
+var criticalConnections = function (n, connections) {
   let edgeMap = {};
-  for (let i = 0; i < n; i++)
-    edgeMap[i] = [];
-  for (let [a,b] of connections)
-    edgeMap[a].push(b), edgeMap[b].push(a);
-  let disc = new Array(n), low = new Array(n), time = 1, criticalConnection = [];
+  for (let i = 0; i < n; i++) edgeMap[i] = [];
+  for (let [a, b] of connections) edgeMap[a].push(b), edgeMap[b].push(a);
+  let disc = new Array(n),
+    low = new Array(n),
+    time = 1,
+    criticalConnection = [];
   const dfs = (curr, prev) => {
     disc[curr] = low[curr] = time++;
     for (let next of edgeMap[curr]) {
       if (!disc[next]) {
         dfs(next, curr);
         low[curr] = Math.min(low[curr], low[next]);
-      } else if (next !== prev){
+      } else if (next !== prev) {
         low[curr] = Math.min(low[curr], disc[next]);
       }
-      if (low[next] > disc[curr]){
+      if (low[next] > disc[curr]) {
         criticalConnection.push([curr, next]);
       }
     }
-  }
+  };
   dfs(0, -1);
   return criticalConnection;
 };
