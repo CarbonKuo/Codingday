949fd74fb4e6ae8d3641b2b522d0b2cd0c429eef
Carbon Kuo <carbonkuo@gmail.com>
2022-08-04 21:47:31 +0800

    Add new solution to 0014.Longest Common Prefix

--- LeetCode/0014.Longest Common Prefix/index.js
+++ LeetCode/0014.Longest Common Prefix/index.js
@@ -1,12 +1,27 @@
 'use strict';
 
-module.exports = { longestCommonPrefix };
+module.exports = { longestCommonPrefix, longestCommonPrefix2 };
 
 /**
  * @param {string[]} strs
  * @return {string} prefix
  */
 function longestCommonPrefix(strs) {
+  if (strs.length === 0) return '';
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, prefix.length - 1);
+    }
+  }
+  return prefix;
+}
+
+/**
+ * @param {string[]} strs
+ * @return {string} prefix
+ */
+function longestCommonPrefix2(strs) {
   let stringElements = strs.length;
   if (stringElements === 0) return '';
   if (stringElements === 1) return strs[0];
