cd10de25186d2e7b0e2dbc89052426b765fb3850
Carbon Kuo <carbonkuo@gmail.com>
2022-09-15 22:43:00 +0800

    Change variable name

--- LeetCode/0200.Number of Islands/index.js
+++ LeetCode/0200.Number of Islands/index.js
@@ -7,26 +7,33 @@ module.exports = { numIslands };
  * @return {number} count
  */
 function numIslands(grid) {
-  let heightOfIsland = grid.length;
-  let wdithOfIsland = grid[0].length;
+  const Island = grid;
+  const heightOfIsland = Island.length;
+  const wdithOfIsland = Island[0].length;
   let count = 0;
   for (let height = 0; height < heightOfIsland; height++) {
     for (let wdith = 0; wdith < wdithOfIsland; wdith++) {
-      if (grid[height][wdith] === '1') {
+      if (Island[height][wdith] === '1') {
         count++;
-        sinkIsland(height, wdith, grid, heightOfIsland, wdithOfIsland);
+        sinkIsland(height, wdith, Island);
       }
     }
   }
   return count;
 }
 
-function sinkIsland(height, wdith, grid, heightOfIsland, wdithOfIsland) {
-  if (height < 0 || height >= heightOfIsland || wdith < 0 || wdith >= wdithOfIsland || grid[height][wdith] === '0')
-    return;
-  grid[height][wdith] = '0';
-  sinkIsland(height, wdith + 1, grid, heightOfIsland, wdithOfIsland);
-  sinkIsland(height, wdith - 1, grid, heightOfIsland, wdithOfIsland);
-  sinkIsland(height + 1, wdith, grid, heightOfIsland, wdithOfIsland);
-  sinkIsland(height - 1, wdith, grid, heightOfIsland, wdithOfIsland);
+function sinkIsland(height, wdith, Island) {
+  if (
+    height < 0 ||
+    height === Island.length ||
+    wdith < 0 ||
+    wdith === Island[0].length ||
+    Island[height][wdith] === '0'
+  )
+    return -1;
+  Island[height][wdith] = '0';
+  sinkIsland(height, wdith + 1, Island);
+  sinkIsland(height, wdith - 1, Island);
+  sinkIsland(height + 1, wdith, Island);
+  sinkIsland(height - 1, wdith, Island);
 }
