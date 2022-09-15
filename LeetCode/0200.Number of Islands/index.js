8935024f3733232c486b37d4226aa51cfd0342e2
Carbon Kuo <carbonkuo@gmail.com>
2022-09-15 22:56:30 +0800

    Fix typo "width"

--- LeetCode/0200.Number of Islands/index.js
+++ LeetCode/0200.Number of Islands/index.js
@@ -9,31 +9,31 @@ module.exports = { numIslands };
 function numIslands(grid) {
   const Island = grid;
   const heightOfIsland = Island.length;
-  const wdithOfIsland = Island[0].length;
+  const widthOfIsland = Island[0].length;
   let count = 0;
   for (let height = 0; height < heightOfIsland; height++) {
-    for (let wdith = 0; wdith < wdithOfIsland; wdith++) {
-      if (Island[height][wdith] === '1') {
+    for (let width = 0; width < widthOfIsland; width++) {
+      if (Island[height][width] === '1') {
         count++;
-        sinkIsland(height, wdith, Island);
+        sinkIsland(height, width, Island);
       }
     }
   }
   return count;
 }
 
-function sinkIsland(height, wdith, Island) {
+function sinkIsland(height, width, Island) {
   if (
     height < 0 ||
     height === Island.length ||
-    wdith < 0 ||
-    wdith === Island[0].length ||
-    Island[height][wdith] === '0'
+    width < 0 ||
+    width === Island[0].length ||
+    Island[height][width] === '0'
   )
     return -1;
-  Island[height][wdith] = '0';
-  sinkIsland(height, wdith + 1, Island);
-  sinkIsland(height, wdith - 1, Island);
-  sinkIsland(height + 1, wdith, Island);
-  sinkIsland(height - 1, wdith, Island);
+  Island[height][width] = '0';
+  sinkIsland(height, width + 1, Island);
+  sinkIsland(height, width - 1, Island);
+  sinkIsland(height + 1, width, Island);
+  sinkIsland(height - 1, width, Island);
 }
