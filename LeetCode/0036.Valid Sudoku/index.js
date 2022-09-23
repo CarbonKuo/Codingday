b0587ec7d8b462a154b6c3007291334ca76c03f4
Carbon Kuo <carbonkuo@gmail.com>
2022-09-23 21:46:08 +0800

    Enhance readability

--- LeetCode/0036.Valid Sudoku/index.js
+++ LeetCode/0036.Valid Sudoku/index.js
@@ -7,23 +7,25 @@ module.exports = { isValidSudoku };
  * @return {boolean}
  */
 function isValidSudoku(board) {
-  let row = new Set();
-  let col = new Set();
-  let box = new Set();
+  const row = new Set();
+  const col = new Set();
+  const box = new Set();
 
   for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
     for (let colIndex = 0; colIndex < 9; colIndex++) {
       if (board[rowIndex][colIndex] === '.') continue;
 
-      let boxIndex = Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3);
+      const boxIndex = Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3);
+      const rowValue = `${rowIndex} ${board[rowIndex][colIndex]}`;
+      const colValue = `${colIndex} ${board[rowIndex][colIndex]}`;
+      const boxValue = `${boxIndex} ${board[rowIndex][colIndex]}`;
 
-      if (row.has(rowIndex + board[rowIndex][colIndex])) return false;
-      if (col.has(colIndex + board[rowIndex][colIndex])) return false;
-      if (box.has(boxIndex + board[rowIndex][colIndex])) return false;
-
-      row.add(rowIndex + board[rowIndex][colIndex]);
-      col.add(colIndex + board[rowIndex][colIndex]);
-      box.add(boxIndex + board[rowIndex][colIndex]);
+      if (row.has(rowValue)) return false;
+      row.add(rowValue);
+      if (col.has(colValue)) return false;
+      col.add(colValue);
+      if (box.has(boxValue)) return false;
+      box.add(boxValue);
     }
   }
   return true;
