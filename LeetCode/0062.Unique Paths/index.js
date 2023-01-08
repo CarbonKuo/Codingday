ceca24347b965362f676391a509d9e517dc3dc82
Carbon Kuo <carbonkuo@gmail.com>
2023-01-08 11:53:14 +0800

    add 0062.Unique Paths sol.

--- LeetCode/0062.Unique Paths/index.js
+++ LeetCode/0062.Unique Paths/index.js
@@ -7,7 +7,7 @@ module.exports = { uniquePaths };
  * @param {number} n
  * @return {number}
  */
-function uniquePaths(m, n) {
+function uniquePaths2(m, n) {
   const dp = Array(n).fill(1);
   while (--m > 0) {
     for (let i = 1; i < n; i++) {
@@ -15,4 +15,20 @@ function uniquePaths(m, n) {
     }
   }
   return dp[n - 1];
+}
+
+function uniquePaths(m, n) {
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
 }
